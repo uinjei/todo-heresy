5d16ac0094415b5df7d64da446ad0af518682d5b
U-USER\ej004418119 <EJ004418119@CUB02ITDDS12209.user.global.local>
2019-08-30 18:11:32 -0400

    additional commit

--- src/template/footer.js
+++ src/template/footer.js
@@ -3,10 +3,8 @@ export default {
     render() {
         this.html`
             <div class="content has-text-centered">
-                <p>
-                <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-                <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-                is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+                <p>Made with ❤️ by <strong>uinjei</strong>
+                special thanks to <a href="https://github.com/jgthms/bulma"><strong>Bulma</strong></a> and <a href="https://github.com/WebReflection/heresy"><strong>Heresy</strong></a>.
                 </p>
             </div>
         `;
